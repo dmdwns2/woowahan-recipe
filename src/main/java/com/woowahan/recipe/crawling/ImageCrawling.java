3f5f91fa98f131b4acefbbe6752cc8f056cf1f0a
이소영 <th42500@naver.com>
2023-01-19 17:03:48 +0900

    [Rename] Crawling Controller 이름 변경

--- src/main/java/com/woowahan/recipe/crawling/ImageCrawling.java
+++ src/main/java/com/woowahan/recipe/crawling/ImageCrawling.java
@@ -1,9 +1,27 @@
 package com.woowahan.recipe.crawling;
 
+import com.woowahan.recipe.domain.dto.Response;
+import com.woowahan.recipe.service.CrawlingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/crawling")
 public class ImageCrawling {
 
-    public void parsing() {
-        String ingredient = "";  // 재료명을 담을 변수
-        // WebElement stuffArea = driver.find
+    private final CrawlingService crawlingService;
+
+    @GetMapping
+    public Response<String> getIngredients() throws IOException {
+        HashSet<String> ingredientHash = crawlingService.ingredientCrawling();
+        crawlingService.productCrawling(ingredientHash);
+//        crawlingService.productCrawling(new HashSet<String>());
+        return Response.success("크롤링 성공 완료");
     }
 }
